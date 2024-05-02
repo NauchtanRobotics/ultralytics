7a5e034f7d5e788313c7e36a28a4e8f54465750a
david <davidmacnaugh@gmail.com>
2024-05-02 15:03:07 +1000

    Default to yolov8s-sev instead of yolov8n-sev.

--- run_sev_tests.py
+++ run_sev_tests.py
@@ -9,20 +9,20 @@ from ultralytics import YOLO
 
 def test_sev():
     # Create a new YOLOv8n-OBB model from scratch
-    model = YOLO('yolov8-sev.yaml', task="sev")
+    model = YOLO('yolov8s-sev.yaml', task="sev")
 
     data_yaml = Path("/home/david/production/severity42.2/dataset.yaml")
     # TODO: problem in ultralytics.data.augment.RandomPerspective.apply_bboxes assumes 4 points not 5. How
     #       do we avoid augmenting the severity field but still have it included?
     results = model.train(
         data=data_yaml,
-        epochs=20,
+        epochs=300,
         imgsz=800,
         cache="RAM",
         device="1",  # "0,1",
         workers=6,
         name="srd42.2",
-        batch=24,
+        batch=16,
         project=Path("/home/david/production/sealed_roads_dataset/.train")  # or could put it in "runs/severity/train"
     )
 
